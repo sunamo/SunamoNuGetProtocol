88845fe4dc254334d5b0984ce944de1048439ac5
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 11:16:10 +0200

    Some projects are flat and buildable

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -5,3 +5,8 @@ global using System.Threading;
 global using NuGet.Common;
 global using NuGet.Protocol;
 global using NuGet.Protocol.Core.Types;
+global using System.Text;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.IO;
+
