22b37d5e893de50aa0e0a7f0c6256c03b76fe05b
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:23:23 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoNuGetProtocol/NuGetProtocolHelper.cs
+++ SunamoNuGetProtocol/NuGetProtocolHelper.cs
@@ -1,6 +1,7 @@
+namespace SunamoNuGetProtocol;
+
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-namespace SunamoNuGetProtocol;
 //
 public class NuGetProtocolHelper
 {
